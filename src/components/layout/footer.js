4b3948bd12df6eb2f318895a7337d01989095bf5
santiago.arcilag <santiago.arcilag@gmail.com>
2020-11-15 21:02:52 -0500

    footer fix

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,14 +1,13 @@
+import Image from 'next/image';
 import styles from './footer.module.scss';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+      <a href="https://linkedin.com/in/sanarguz" target="_blank">
+        <Image src="/img/linkedin.png" width="80" height="80" />
+      </a>
+      <a href="https://github.com/sanarguz" target="_blank">
+        <Image src="/img/github.png" width="80" height="80" />
       </a>
     </footer>
   );
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -6,25 +6,6 @@ export default function Header() {
         <h2>
           <span>Mi</span> Portafolio
         </h2>
-        <nav>
-          <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#acerca">Acerca de mi </a>
-            </li>
-            <li>
-              <a href="#tecnologias">Tecnologias</a>
-            </li>
-            <li>
-              <a href="#proeyctos">Proyectos</a>
-            </li>
-            <li>
-              <a href="#contacto">Contacto</a>
-            </li>
-          </ul>
-        </nav>
       </div>
     </header>
   );
